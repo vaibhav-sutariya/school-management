0da114080bc17b32fe867ad18e297b7a2c4f3dd8
vaibhav-infynno <vaibhav.infynno@gmail.com>
2026-01-22 13:01:06 +0530

    color

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ import 'cubit/internet/internet_cubit.dart';
 import 'cubit/locale_cubit.dart';
 import 'cubit/theme_cubit.dart';
 import 'l10n/l10n.dart';
+// 491B6D
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
