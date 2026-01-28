485d935b8329a2a2276e7230b45085a98d0c04ca
vaibhav-infynno <vaibhav.infynno@gmail.com>
2026-01-28 16:22:21 +0530

    refactor: remove unused imports for forgot password, new password, and password success pages.

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,10 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:starter_app/core/routes/app_router.gr.dart';
 
-import '../../features/auth/screens/forgot_password/ui/forgot_password_page.dart';
-import '../../features/auth/screens/forgot_password/ui/new_password_page.dart';
-import '../../features/auth/screens/forgot_password/ui/password_success_page.dart';
-import 'app_router.gr.dart';
 import 'auth_guard.dart';
 
 @AutoRouterConfig()
